5a5e7a02e8e0b647f1c60b745886f110b1d7f94a
charonko-LANL <jcharonk@lanl.gov>
2019-05-03 23:05:40 -0600

    Add DaVis style calibration to camera angle calculation

--- stereo_angle_uncertainty.m
+++ stereo_angle_uncertainty.m
@@ -145,7 +145,22 @@ elseif modeltype==2
         dFdx3(:,:,gg) = a(4) + a(8)*xg + a(9)*yg + 2*a(10)*zg + a(15)*xg.^2 + a(16)*xg.*yg + ...
             a(17)*yg.^2 + 2*a(18)*xg.*zg + 2*a(19)*yg.*zg;
     end
-    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Mapping the camera coord. to the World Coord. using linear interp between cubic xy planes
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for gg=1:4
+        a=aall(:,gg);
+        dFdx1(:,:,gg) = a(2) + 2*a(5)*xg + a(6)*yg + a(8)*zg + 3*a(10)*xg.^2 + ...
+            2*a(11)*xg.*yg + a(12)*yg.^2 + 2*a(14)*xg.*zg + a(15)*yg.*zg + ...
+            2*a(17)*xg.*yg.*zg + a(18)*yg.^2.*zg + 3*a(19)*xg.^2.*zg;
+
+        dFdx2(:,:,gg) = a(3) + a(6)*xg + 2*a(7)*yg + a(9)*zg + a(11)*xg.^2 + ...
+            2*a(12)*xg.*yg + 3*a(13)*yg.^2 + a(15)*xg.*zg + 2*a(16)*yg.*zg + ...
+            a(17)*xg.^2.*zg + 2*a(18)*xg.*yg.*zg + 3*a(20)*yg.^2.*zg;
+
+        dFdx3(:,:,gg) = a(4) + a(8)*xg + a(9)*yg + a(14)*xg.^2 + a(15)*xg.*yg + a(16)*yg.^2 + ...
+            a(17)*xg.^2.*yg + a(18)*xg.*yg.^2 + a(19)*xg.^3 + a(20)*yg.^3;
+    end
 end
 
 
